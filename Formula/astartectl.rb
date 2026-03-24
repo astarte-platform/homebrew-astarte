eb8e3633ee27cc63e5b2f935bb6fee9f1afce5fd
Joshua Chapman <joshua.chapman@secomind.com>
2026-03-24 17:03:29 +0100

    ci(token): fix typo in token template
    Signed-off-by: Joshua Chapman <joshua.chapman@secomind.com>

--- Formula/astartectl.rb
+++ Formula/astartectl.rb
@@ -1,7 +1,7 @@
 class Astartectl < Formula
   desc "Astarte command-line client utility"
   homepage "https://astarte.cloud"
-  url "https://github.com/astarte-platform/astartectl/archive/v22.11.00.tar.gz"
+  url "https://github.com/astarte-platform/astartectl/archive/refs/tags/v22.11.00.tar.gz"
   sha256 "4e5eaf5c4215424d3bf420f601d426cdb0c2b069ae46f248aefe56dfa9440640"
 
   depends_on "go" => :build
@@ -10,15 +10,9 @@ class Astartectl < Formula
     system "go", "build"
     bin.install "astartectl" => "astartectl"
 
-    # Install bash completion
-    output = Utils.popen_read("#{bin}/astartectl completion bash")
-    (bash_completion/"astartectl").write output
-
-    # Install zsh completion
-    output = Utils.popen_read("#{bin}/astartectl completion zsh")
-    (zsh_completion/"_astartectl").write output
-
     prefix.install_metafiles
+
+    generate_completions_from_executable("#{bin}/astartectl", "completion")
   end
 
   test do
