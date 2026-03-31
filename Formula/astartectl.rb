1df774279863bd209425b612517cfe93741898e9
Arnaldo Cesco <arnaldo.cesco@secomind.com>
2026-03-31 12:19:05 +0000

    astartectl: add 24.5.3 bottle.

--- Formula/astartectl.rb
+++ Formula/astartectl.rb
@@ -4,6 +4,12 @@ class Astartectl < Formula
   url "https://github.com/astarte-platform/astartectl/archive/refs/tags/v24.5.3.tar.gz"
   sha256 "c34a66600362f87efbd5d074dff0214a5cb723e409ba4de967c703a7f9efebd5"
 
+  bottle do
+    root_url "https://ghcr.io/v2/astarte-platform/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "3c572bedf678caa6ab3ea3021266bd754ea99bc0b54721ebd23d3ecdcf50b9a4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c6d3695d6994e5041b07522770f955950489aed81d49f37e36d044fda314bf4a"
+  end
+
   depends_on "go" => :build
 
   def install
